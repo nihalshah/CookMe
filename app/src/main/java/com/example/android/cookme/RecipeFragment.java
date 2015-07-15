01da4493f00bb0d5776f38fceac22662ee5e7740
Nihal <nsnihalshah@gmail.com>
2015-07-15 13:00:44 -0500

    Fixed Error in RecipeProvider Contructor

--- app/src/main/java/com/example/android/cookme/RecipeFragment.java
+++ app/src/main/java/com/example/android/cookme/RecipeFragment.java
@@ -2,15 +2,16 @@ package com.example.android.cookme;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.android.cookme.data.RecipeProvider;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 /**
@@ -28,28 +29,24 @@ public class RecipeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-//
-//        //mListRecipes = new RecipeProvider(getActivity());
-//
-//        //Fake Data to try the population of the ListView with an ArrayList of Strings
-//        String [] fakeData = {"Pizza with Brocoli", "Tune Sandwich", "Paella",
-//                                "Banana MilkShake", "Black Coffee", "Scramble Eggs",
-//                                "French Bread", "Mexican Tacos"};
-//
-//        ArrayList<String> fakeListRecipes = new ArrayList<String>(Arrays.asList(fakeData));
-//
-//        mRecipeAdapter = new ArrayAdapter<>(
-//                                    getActivity(),
-//                                    R.layout.list_item_recipes,
-//                                    R.id.list_item_recipes_textview,
-//                                    fakeListRecipes);
-//
-//        ListView listRecipes = (ListView) rootView.findViewById(R.id.recipes_list);
-//        listRecipes.setAdapter(mRecipeAdapter);
 
-        RecipeProvider obj = new RecipeProvider(getActivity());
-        ArrayList<String> allRecipes = obj.getRecipeNames();
-        Log.v("First Recipe :", allRecipes.get(0));
+        //mListRecipes = new RecipeProvider(getActivity());
+
+        //Fake Data to try the population of the ListView with an ArrayList of Strings
+        String [] fakeData = {"Pizza with Brocoli", "Tune Sandwich", "Paella",
+                                "Banana MilkShake", "Black Coffee", "Scramble Eggs",
+                                "French Bread", "Mexican Tacos"};
+
+        ArrayList<String> fakeListRecipes = new ArrayList<String>(Arrays.asList(fakeData));
+
+        mRecipeAdapter = new ArrayAdapter<>(
+                                    getActivity(),
+                                    R.layout.list_item_recipes,
+                                    R.id.list_item_recipes_textview,
+                                    fakeListRecipes);
+
+        ListView listRecipes = (ListView) rootView.findViewById(R.id.recipes_list);
+        listRecipes.setAdapter(mRecipeAdapter);
 
         return rootView;
     }
