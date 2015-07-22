91a400c7f710eb5e053b2dfd4f1d8635015d490a
EduardoVaca <vacalalo@gmail.com>
2015-07-22 12:11:06 -0500

    Added PollingCheck class for testing

--- app/src/androidTest/java/com/example/android/cookme/data/TestUtilities.java
+++ app/src/androidTest/java/com/example/android/cookme/data/TestUtilities.java
@@ -1,8 +1,14 @@
 package com.example.android.cookme.data;
 
 import android.content.ContentValues;
+import android.database.ContentObserver;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.test.AndroidTestCase;
+
+import com.example.android.cookme.utils.PollingCheck;
 
 import java.util.Map;
 import java.util.Set;
@@ -52,5 +58,51 @@ public class TestUtilities extends AndroidTestCase{
         testValues.put(RecipeContract.RecipeIngredientRelationship.COL_UNITS, "CUP");
         testValues.put(RecipeContract.RecipeIngredientRelationship.COL_QUANTITY, 1);
         return testValues;
+    }
+
+
+    static class TestContentObserver extends ContentObserver {
+        final HandlerThread mHT;
+        boolean mContentChanged;
+
+        static TestContentObserver getTestContentObserver() {
+            HandlerThread ht = new HandlerThread("ContentObserverThread");
+            ht.start();
+            return new TestContentObserver(ht);
+        }
+
+        private TestContentObserver(HandlerThread ht) {
+            super(new Handler(ht.getLooper()));
+            mHT = ht;
+        }
+
+        // On earlier versions of Android, this onChange method is called
+        @Override
+        public void onChange(boolean selfChange) {
+            onChange(selfChange, null);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            mContentChanged = true;
+        }
+
+        public void waitForNotificationOrFail() {
+            // Note: The PollingCheck class is taken from the Android CTS (Compatibility Test Suite).
+            // It's useful to look at the Android CTS source for ideas on how to test your Android
+            // applications.  The reason that PollingCheck works is that, by default, the JUnit
+            // testing framework is not running on the main Android application thread.
+            new PollingCheck(5000) {
+                @Override
+                protected boolean check() {
+                    return mContentChanged;
+                }
+            }.run();
+            mHT.quit();
+        }
+    }
+
+    static TestContentObserver getTestContentObserver() {
+        return TestContentObserver.getTestContentObserver();
     }
 }
