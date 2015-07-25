fe4c2bbe6a1a1d382141cea269a442556239e05a
Javier A. Suarez Rivas <jsuarez@go.olemiss.edu>
2015-07-24 19:45:31 -0500

    First commit for the new activity add Recipe Layout

--- app/src/main/java/com/example/android/cookme/addRecipeActivity.java
+++ app/src/main/java/com/example/android/cookme/addRecipeActivity.java
@@ -1,26 +1,25 @@
 package com.example.android.cookme;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
+import com.example.android.cookme.R;
 
-public class MainActivity extends ActionBarActivity {
+public class addRecipeActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_recipe);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_add_recipe, menu);
         return true;
     }
 
@@ -37,11 +36,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void sendMessage(View view)
-    {
-        Intent intent = new Intent(MainActivity.this, addRecipeActivity.class);
-        startActivity(intent);
     }
 }
