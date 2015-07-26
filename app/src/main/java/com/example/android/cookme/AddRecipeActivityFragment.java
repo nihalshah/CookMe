612aaa99b1d03d45f569b14dd7a631e102dafad5
Javier A. Suarez Rivas <jsuarez@go.olemiss.edu>
2015-07-26 16:58:40 -0500

    Added camara intent to take pictures, and display picture as thumbnail

--- app/src/main/java/com/example/android/cookme/AddRecipeActivityFragment.java
+++ app/src/main/java/com/example/android/cookme/AddRecipeActivityFragment.java
@@ -1,21 +1,40 @@
 package com.example.android.cookme;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.cookme.R;
+
+import java.io.File;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class AddRecipeActivityFragment extends Fragment {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView mImageView;
+    private Uri mURI;
+    private static final int PICK_IMAGE = 0;
+    private static final int PICK_IMAGE_FROM_GALLERY = 1;
+
 
     public AddRecipeActivityFragment() {
     }
@@ -25,6 +44,18 @@ public class AddRecipeActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_add_recipe, container, false);
 
+        mImageView = (ImageView) rootView.findViewById(R.id.add_picture_thumbnail);
+
+        Button takePictureButton = (Button) rootView.findViewById(R.id.add_picture_button);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, 1888);
+            }
+
+
+        });
 
         /*Button event*/
 
@@ -49,7 +80,7 @@ public class AddRecipeActivityFragment extends Fragment {
                 String instructions = instructionsInput.getText().toString();
 
                 Utility.insertWholeRecipeInDb(getActivity(), recipe_name, instructions,
-                                                ingredient_name, units, quantity);
+                        ingredient_name, units, quantity);
 
                 //Message that shows the user that his recipe was added
                 AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getActivity()).
@@ -61,4 +92,17 @@ public class AddRecipeActivityFragment extends Fragment {
         });
         return rootView;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.e("LOG", "" + requestCode);
+        if (requestCode == 1888 && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            mImageView.setImageBitmap(photo);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
