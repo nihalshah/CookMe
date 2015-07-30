c2cd7ddc8555eced5a5e84fe89ec13e7d73d3a7b
EduardoVaca <vacalalo@gmail.com>
2015-07-30 17:25:14 -0500

    Add ingredient button working, it stores ingredient in arrayList and update textview

--- app/src/main/java/com/example/android/cookme/AddRecipeActivityFragment.java
+++ app/src/main/java/com/example/android/cookme/AddRecipeActivityFragment.java
@@ -22,8 +22,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.cookme.R;
+import com.example.android.cookme.data.Ingredient;
 
 import java.io.File;
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -35,6 +37,8 @@ public class AddRecipeActivityFragment extends Fragment {
     private Uri mURI;
     private static final int PICK_IMAGE = 0;
     private static final int PICK_IMAGE_FROM_GALLERY = 1;
+    private String mIngredientAdded;
+    private ArrayList<Ingredient> mIngredientsList;
 
 
     public AddRecipeActivityFragment() {
@@ -47,6 +51,11 @@ public class AddRecipeActivityFragment extends Fragment {
 
         mImageView = (ImageView) rootView.findViewById(R.id.add_picture_imageview);
 
+        mIngredientAdded = "INGREDIENTS ADDED : ";
+        mIngredientsList = new ArrayList<>();
+
+        final TextView ingredientsAdded_textview = (TextView)rootView.findViewById(R.id.list_of_ingredients_added);
+
         Button takePictureButton = (Button) rootView.findViewById(R.id.add_picture_button);
         takePictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,15 +67,13 @@ public class AddRecipeActivityFragment extends Fragment {
 
         });
 
-        /*Button event*/
 
-        Button addButton = (Button) rootView.findViewById(R.id.add_new_recipe_button);
-        addButton.setOnClickListener(new View.OnClickListener() {
+        /*Add ingredient button event*/
+
+        Button addIngredientButton = (Button) rootView.findViewById(R.id.add_ingredient_button);
+        addIngredientButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                EditText recipeInput = (EditText) rootView.findViewById(R.id.add_recipe_name_input);
-                String recipe_name = recipeInput.getText().toString();
 
                 EditText ingredientInput = (EditText) rootView.findViewById(R.id.add_ingredient_name_input);
                 String ingredient_name = ingredientInput.getText().toString();
@@ -77,6 +84,30 @@ public class AddRecipeActivityFragment extends Fragment {
                 EditText quantityInput = (EditText) rootView.findViewById(R.id.add_quantity_input);
                 double quantity = Double.parseDouble(quantityInput.getText().toString());
 
+                mIngredientsList.add(new Ingredient(ingredient_name, quantity, units));
+
+                mIngredientAdded += ingredient_name + " " + quantity + " " + units + ", ";
+                ingredientsAdded_textview.setText(mIngredientAdded);
+
+                ingredientInput.setText("");
+                unitsInput.setText("");
+                quantityInput.setText("");
+
+            }
+        });
+
+        /*Add recipe Button event*/
+
+        Button addButton = (Button) rootView.findViewById(R.id.add_new_recipe_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText recipeInput = (EditText) rootView.findViewById(R.id.add_recipe_name_input);
+                String recipe_name = recipeInput.getText().toString();
+
+
+
                 EditText instructionsInput = (EditText) rootView.findViewById(R.id.add_instruction_input);
                 String instructions = instructionsInput.getText().toString();
 
@@ -86,8 +117,8 @@ public class AddRecipeActivityFragment extends Fragment {
 
                 byte picture_in_bytes[] = Utility.getBytes(picture);
 
-                Utility.insertWholeRecipeInDb(getActivity(), recipe_name, instructions,
-                        picture_in_bytes, ingredient_name, units, quantity);
+                /*Utility.insertWholeRecipeInDb(getActivity(), recipe_name, instructions,
+                        picture_in_bytes, ingredient_name, units, quantity);*/
 
                 //Message that shows the user that his recipe was added
                 AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getActivity()).
