7e32a6aa95aca49cef869c69105a13ee49276c60
Javier A. Suarez Rivas <jsuarez@go.olemiss.edu>
2015-08-06 20:02:24 -0500

    Added a way to dynamically set the height of the listview based on the number of ingredients

--- app/src/main/java/com/example/android/cookme/DetailActivityFragment.java
+++ app/src/main/java/com/example/android/cookme/DetailActivityFragment.java
@@ -21,6 +21,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
@@ -205,6 +206,8 @@ public class DetailActivityFragment extends Fragment implements LoaderCallbacks<
         );
 
         mIngredientListView.setAdapter(ingredientAdapter);
+        //TO DO CHECK IF FALSE;
+        setListViewHeightBasedOnItems(mIngredientListView);
 
         mShareString += "\n\nSteps: \n" + instructions + "\n\n" + HASHTAG;
         mShareString += "\n" + "https://www.facebook.com";
@@ -245,5 +248,44 @@ public class DetailActivityFragment extends Fragment implements LoaderCallbacks<
                 });
         AlertDialog alert = builder.create();
         alert.show();
+    }
+
+    /**
+     * Sets ListView height dynamically based on the height of the items.
+     *
+     * @param listView to be resized
+     * @return true if the listView is successfully resized, false otherwise
+     */
+    public static boolean setListViewHeightBasedOnItems(ListView listView) {
+
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter != null) {
+
+            int numberOfItems = listAdapter.getCount();
+
+            // Get total height of all items.
+            int totalItemsHeight = 0;
+            for (int itemPos = 0; itemPos < numberOfItems; itemPos++) {
+                View item = listAdapter.getView(itemPos, null, listView);
+                item.measure(0, 0);
+                totalItemsHeight += item.getMeasuredHeight();
+            }
+
+            // Get total height of all item dividers.
+            int totalDividersHeight = listView.getDividerHeight() *
+                    (numberOfItems - 1);
+
+            // Set list height.
+            ViewGroup.LayoutParams params = listView.getLayoutParams();
+            params.height = totalItemsHeight + totalDividersHeight;
+            listView.setLayoutParams(params);
+            listView.requestLayout();
+
+            return true;
+
+        } else {
+            return false;
+        }
+
     }
 }
