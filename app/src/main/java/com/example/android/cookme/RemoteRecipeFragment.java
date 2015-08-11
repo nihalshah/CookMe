d89f724a05c06ae88ff02436f7bf0ae0542ff74b
Nihal <nsnihalshah@gmail.com>
2015-08-11 10:36:52 -0700

    Remote Search works

--- app/src/main/java/com/example/android/cookme/RemoteRecipeFragment.java
+++ app/src/main/java/com/example/android/cookme/RemoteRecipeFragment.java
@@ -111,6 +111,8 @@ public class RemoteRecipeFragment extends Fragment {
 
                     //TODO Search into remote function
 
+                    new RemoteRecipeTask().execute("Search", mIngredientInput.getText().toString());
+
                     InputMethodManager inputManager =
                             (InputMethodManager) getActivity().
                                     getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -208,7 +210,7 @@ public class RemoteRecipeFragment extends Fragment {
                 String base = "http://45.55.139.196:5000/";
                 String buildUri;
                 if( params[0].equals("Search")) {
-                    buildUri = base.concat("/ingredient/"+params[1]);
+                    buildUri = base.concat("ingredient/"+params[1]);
                 }
                 else{
                     buildUri = base;
